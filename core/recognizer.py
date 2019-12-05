58c968a8877c1de49c5a0396bee5a8e1d9987e71
gengzhikui <gengzhikui@gmail.com>
2019-12-05 10:30:33 +0800

    Update recognizer.py
    support face++ 106 points

--- core/recognizer.py
+++ core/recognizer.py
@@ -16,21 +16,21 @@ NOSE_POINTS = list(range(55, 65))
 RIGHT_EYE_POINTS = list(range(65, 75))
 RIGHT_BROW_POINTS = list(range(75, 83))
 """
-FACE_POINTS = list(range(0, 78))
-JAW_POINTS = list(range(0, 19))
-LEFT_EYE_POINTS = list(range(19, 29))
-LEFT_BROW_POINTS = list(range(29, 37))
-NOSE_POINTS = list(range(37, 47))
-RIGHT_EYE_POINTS = list(range(47, 57))
-RIGHT_BROW_POINTS = list(range(57, 65))
-MOUTH_POINTS = list(range(65, 78))
+FACE_POINTS = list(range(0, 94))
+JAW_POINTS = list(range(0, 33))
+LEFT_EYE_POINTS = list(range(33, 41))
+LEFT_BROW_POINTS = list(range(41, 50))
+NOSE_POINTS = list(range(50, 65))
+RIGHT_EYE_POINTS = list(range(65, 73))
+RIGHT_BROW_POINTS = list(range(73, 82))
+MOUTH_POINTS = list(range(82, 94))
 
-LEFT_FACE = list(range(0, 10)) + list(range(29, 34))
-RIGHT_FACE = list(range(9, 19)) + list(range(57, 62))
+LEFT_FACE = list(range(0, 17)) + list(range(42, 46))
+RIGHT_FACE = list(range(16, 33)) + list(range(74, 78))
 
-JAW_END = 19
+JAW_END = 33
 FACE_START = 0
-FACE_END = 78
+FACE_END = 94
 
 OVERLAY_POINTS = [
     LEFT_FACE,
@@ -70,7 +70,7 @@ def landmarks_by_face__(image):
     params = {
         'api_key': 'oVnmrGYMAqUnGSSMf01mOW3rfRmWj52U',
         'api_secret': 'pumbBbjNuDUXY18H2r4kQkuoOQLpI3nQ',
-        'return_landmark': 1,
+        'return_landmark': 2,
     }
     file = {'image_file': open(image, 'rb')}
 
@@ -98,114 +98,101 @@ def matrix_rectangle(left, top, width, height):
 
 
 def matrix_marks(res):
-    pointer = [
-        [res['contour_left1']['x'], res['contour_left1']['y']],
-        [res['contour_left2']['x'], res['contour_left2']['y']],
-        [res['contour_left3']['x'], res['contour_left3']['y']],
-        [res['contour_left4']['x'], res['contour_left4']['y']],
-        [res['contour_left5']['x'], res['contour_left5']['y']],
-        [res['contour_left6']['x'], res['contour_left6']['y']],
-        [res['contour_left7']['x'], res['contour_left7']['y']],
-        [res['contour_left8']['x'], res['contour_left8']['y']],
-        [res['contour_left9']['x'], res['contour_left9']['y']],
-        [res['contour_chin']['x'], res['contour_chin']['y']],
-        [res['contour_right9']['x'], res['contour_right9']['y']],
-        [res['contour_right8']['x'], res['contour_right8']['y']],
-        [res['contour_right7']['x'], res['contour_right7']['y']],
-        [res['contour_right6']['x'], res['contour_right6']['y']],
-        [res['contour_right5']['x'], res['contour_right5']['y']],
-        [res['contour_right4']['x'], res['contour_right4']['y']],
-        [res['contour_right3']['x'], res['contour_right3']['y']],
-        [res['contour_right2']['x'], res['contour_right2']['y']],
-        [res['contour_right1']['x'], res['contour_right1']['y']],
-
-        [res['left_eye_bottom']['x'], res['left_eye_bottom']['y']],
-        [res['left_eye_center']['x'], res['left_eye_center']['y']],
-        [res['left_eye_left_corner']['x'], res['left_eye_left_corner']['y']],
-        [res['left_eye_lower_left_quarter']['x'], res['left_eye_lower_left_quarter']['y']],
-        [res['left_eye_lower_right_quarter']['x'], res['left_eye_lower_right_quarter']['y']],
-        [res['left_eye_pupil']['x'], res['left_eye_pupil']['y']],
-        [res['left_eye_right_corner']['x'], res['left_eye_right_corner']['y']],
-        [res['left_eye_top']['x'], res['left_eye_top']['y']],
-        [res['left_eye_upper_left_quarter']['x'], res['left_eye_upper_left_quarter']['y']],
-        [res['left_eye_upper_right_quarter']['x'], res['left_eye_upper_right_quarter']['y']],
-
-        [res['left_eyebrow_left_corner']['x'], res['left_eyebrow_left_corner']['y']],
-        [res['left_eyebrow_upper_left_quarter']['x'], res['left_eyebrow_upper_left_quarter']['y']],
-        [res['left_eyebrow_upper_middle']['x'], res['left_eyebrow_upper_middle']['y']],
-        [res['left_eyebrow_upper_right_quarter']['x'], res['left_eyebrow_upper_right_quarter']['y']],
-        [res['left_eyebrow_right_corner']['x'], res['left_eyebrow_right_corner']['y']],
-        [res['left_eyebrow_lower_left_quarter']['x'], res['left_eyebrow_lower_left_quarter']['y']],
-        [res['left_eyebrow_lower_middle']['x'], res['left_eyebrow_lower_middle']['y']],
-        [res['left_eyebrow_lower_right_quarter']['x'], res['left_eyebrow_lower_right_quarter']['y']],
-
-
-        [res['nose_contour_left1']['x'], res['nose_contour_left1']['y']],
-        [res['nose_contour_left2']['x'], res['nose_contour_left2']['y']],
-        [res['nose_contour_left3']['x'], res['nose_contour_left3']['y']],
-        [res['nose_contour_lower_middle']['x'], res['nose_contour_lower_middle']['y']],
-        [res['nose_contour_right1']['x'], res['nose_contour_right1']['y']],
-        [res['nose_contour_right2']['x'], res['nose_contour_right2']['y']],
-        [res['nose_contour_right3']['x'], res['nose_contour_right3']['y']],
-        [res['nose_left']['x'], res['nose_left']['y']],
-        [res['nose_right']['x'], res['nose_right']['y']],
-        [res['nose_tip']['x'], res['nose_tip']['y']],
-
-        [res['right_eye_bottom']['x'], res['right_eye_bottom']['y']],
-        [res['right_eye_center']['x'], res['right_eye_center']['y']],
-        [res['right_eye_left_corner']['x'], res['right_eye_left_corner']['y']],
-        [res['right_eye_lower_left_quarter']['x'], res['right_eye_lower_left_quarter']['y']],
-        [res['right_eye_lower_right_quarter']['x'], res['right_eye_lower_right_quarter']['y']],
-        [res['right_eye_pupil']['x'], res['right_eye_pupil']['y']],
-        [res['right_eye_right_corner']['x'], res['right_eye_right_corner']['y']],
-        [res['right_eye_top']['x'], res['right_eye_top']['y']],
-        [res['right_eye_upper_left_quarter']['x'], res['right_eye_upper_left_quarter']['y']],
-        [res['right_eye_upper_right_quarter']['x'], res['right_eye_upper_right_quarter']['y']],
-
-        [res['right_eyebrow_left_corner']['x'], res['right_eyebrow_left_corner']['y']],
-        [res['right_eyebrow_upper_left_quarter']['x'], res['right_eyebrow_upper_left_quarter']['y']],
-        [res['right_eyebrow_upper_middle']['x'], res['right_eyebrow_upper_middle']['y']],
-        [res['right_eyebrow_upper_right_quarter']['x'], res['right_eyebrow_upper_right_quarter']['y']],
-        [res['right_eyebrow_right_corner']['x'], res['right_eyebrow_right_corner']['y']],
-        [res['right_eyebrow_lower_left_quarter']['x'], res['right_eyebrow_lower_left_quarter']['y']],
-        [res['right_eyebrow_lower_middle']['x'], res['right_eyebrow_lower_middle']['y']],
-        [res['right_eyebrow_lower_right_quarter']['x'], res['right_eyebrow_lower_right_quarter']['y']],
-
-        [res['mouth_left_corner']['x'], res['mouth_left_corner']['y']],
-        [res['mouth_lower_lip_bottom']['x'], res['mouth_lower_lip_bottom']['y']],
-        [res['mouth_lower_lip_left_contour2']['x'], res['mouth_lower_lip_left_contour2']['y']],
-        [res['mouth_lower_lip_left_contour3']['x'], res['mouth_lower_lip_left_contour3']['y']],
-        [res['mouth_lower_lip_bottom']['x'], res['mouth_lower_lip_top']['y']],
-        [res['mouth_lower_lip_right_contour2']['x'], res['mouth_lower_lip_right_contour2']['y']],
-        [res['mouth_lower_lip_right_contour3']['x'], res['mouth_lower_lip_right_contour3']['y']],
-        [res['mouth_right_corner']['x'], res['mouth_right_corner']['y']],
-        [res['mouth_upper_lip_left_contour1']['x'], res['mouth_upper_lip_left_contour1']['y']],
-        [res['mouth_upper_lip_left_contour2']['x'], res['mouth_upper_lip_left_contour2']['y']],
-        [res['mouth_upper_lip_right_contour1']['x'], res['mouth_upper_lip_right_contour1']['y']],
-        [res['mouth_upper_lip_right_contour2']['x'], res['mouth_upper_lip_right_contour2']['y']],
-        [res['mouth_upper_lip_top']['x'], res['mouth_upper_lip_top']['y']],   
-
-
+    pointer=[
+        [res['contour_left1']['x'],res['contour_left1']['y']],
+        [res['contour_left2']['x'],res['contour_left2']['y']],
+        [res['contour_left3']['x'],res['contour_left3']['y']],
+        [res['contour_left4']['x'],res['contour_left4']['y']],
+        [res['contour_left5']['x'],res['contour_left5']['y']],
+        [res['contour_left6']['x'],res['contour_left6']['y']],
+        [res['contour_left7']['x'],res['contour_left7']['y']],
+        [res['contour_left8']['x'],res['contour_left8']['y']],
+        [res['contour_left9']['x'],res['contour_left9']['y']],
+        [res['contour_left10']['x'],res['contour_left10']['y']],
+        [res['contour_left11']['x'],res['contour_left11']['y']],
+        [res['contour_left12']['x'],res['contour_left12']['y']],
+        [res['contour_left13']['x'],res['contour_left13']['y']],
+        [res['contour_left14']['x'],res['contour_left14']['y']],
+        [res['contour_left15']['x'],res['contour_left15']['y']],
+        [res['contour_left16']['x'],res['contour_left16']['y']],
+        [res['contour_chin']['x'],res['contour_chin']['y']],
+        [res['contour_right16']['x'],res['contour_right16']['y']],
+        [res['contour_right15']['x'],res['contour_right15']['y']],
+        [res['contour_right14']['x'],res['contour_right14']['y']],
+        [res['contour_right13']['x'],res['contour_right13']['y']],
+        [res['contour_right12']['x'],res['contour_right12']['y']],
+        [res['contour_right11']['x'],res['contour_right11']['y']],
+        [res['contour_right10']['x'],res['contour_right10']['y']],
+        [res['contour_right9']['x'],res['contour_right9']['y']],
+        [res['contour_right8']['x'],res['contour_right8']['y']],
+        [res['contour_right7']['x'],res['contour_right7']['y']],
+        [res['contour_right6']['x'],res['contour_right6']['y']],
+        [res['contour_right5']['x'],res['contour_right5']['y']],
+        [res['contour_right4']['x'],res['contour_right4']['y']],
+        [res['contour_right3']['x'],res['contour_right3']['y']],
+        [res['contour_right2']['x'],res['contour_right2']['y']],
+        [res['contour_right1']['x'],res['contour_right1']['y']],
+        [res['left_eye_left_corner']['x'],res['left_eye_left_corner']['y']],
+        [res['left_eye_upper_left_quarter']['x'],res['left_eye_upper_left_quarter']['y']],
+        [res['left_eye_top']['x'],res['left_eye_top']['y']],
+        [res['left_eye_upper_right_quarter']['x'],res['left_eye_upper_right_quarter']['y']],
+        [res['left_eye_right_corner']['x'],res['left_eye_right_corner']['y']],
+        [res['left_eye_lower_right_quarter']['x'],res['left_eye_lower_right_quarter']['y']],
+        [res['left_eye_bottom']['x'],res['left_eye_bottom']['y']],
+        [res['left_eye_lower_left_quarter']['x'],res['left_eye_lower_left_quarter']['y']],
+        [res['left_eyebrow_left_corner']['x'],res['left_eyebrow_left_corner']['y']],
+        [res['left_eyebrow_upper_left_quarter']['x'],res['left_eyebrow_upper_left_quarter']['y']],
+        [res['left_eyebrow_upper_middle']['x'],res['left_eyebrow_upper_middle']['y']],
+        [res['left_eyebrow_upper_right_quarter']['x'],res['left_eyebrow_upper_right_quarter']['y']],
+        [res['left_eyebrow_upper_right_corner']['x'],res['left_eyebrow_upper_right_corner']['y']],
+        [res['left_eyebrow_lower_right_corner']['x'],res['left_eyebrow_lower_right_corner']['y']],
+        [res['left_eyebrow_lower_right_quarter']['x'],res['left_eyebrow_lower_right_quarter']['y']],
+        [res['left_eyebrow_lower_middle']['x'],res['left_eyebrow_lower_middle']['y']],
+        [res['left_eyebrow_lower_left_quarter']['x'],res['left_eyebrow_lower_left_quarter']['y']],
+        [res['nose_bridge1']['x'],res['nose_bridge1']['y']],
+        [res['nose_bridge2']['x'],res['nose_bridge2']['y']],
+        [res['nose_bridge3']['x'],res['nose_bridge3']['y']],
+        [res['nose_tip']['x'],res['nose_tip']['y']],
+        [res['nose_left_contour1']['x'],res['nose_left_contour1']['y']],
+        [res['nose_left_contour2']['x'],res['nose_left_contour2']['y']],
+        [res['nose_left_contour3']['x'],res['nose_left_contour3']['y']],
+        [res['nose_left_contour4']['x'],res['nose_left_contour4']['y']],
+        [res['nose_left_contour5']['x'],res['nose_left_contour5']['y']],
+        [res['nose_middle_contour']['x'],res['nose_middle_contour']['y']],
+        [res['nose_right_contour1']['x'],res['nose_right_contour1']['y']],
+        [res['nose_right_contour2']['x'],res['nose_right_contour2']['y']],
+        [res['nose_right_contour3']['x'],res['nose_right_contour3']['y']],
+        [res['nose_right_contour4']['x'],res['nose_right_contour4']['y']],
+        [res['nose_right_contour5']['x'],res['nose_right_contour5']['y']],
+        [res['right_eye_right_corner']['x'],res['right_eye_right_corner']['y']],
+        [res['right_eye_upper_right_quarter']['x'],res['right_eye_upper_right_quarter']['y']],
+        [res['right_eye_top']['x'],res['right_eye_top']['y']],
+        [res['right_eye_upper_left_quarter']['x'],res['right_eye_upper_left_quarter']['y']],
+        [res['right_eye_left_corner']['x'],res['right_eye_left_corner']['y']],
+        [res['right_eye_lower_left_quarter']['x'],res['right_eye_lower_left_quarter']['y']],
+        [res['right_eye_bottom']['x'],res['right_eye_bottom']['y']],
+        [res['right_eye_lower_right_quarter']['x'],res['right_eye_lower_right_quarter']['y']],
+        [res['right_eyebrow_right_corner']['x'],res['right_eyebrow_right_corner']['y']],
+        [res['right_eyebrow_upper_right_quarter']['x'],res['right_eyebrow_upper_right_quarter']['y']],
+        [res['right_eyebrow_upper_middle']['x'],res['right_eyebrow_upper_middle']['y']],
+        [res['right_eyebrow_upper_left_quarter']['x'],res['right_eyebrow_upper_left_quarter']['y']],
+        [res['right_eyebrow_upper_left_corner']['x'],res['right_eyebrow_upper_left_corner']['y']],
+        [res['right_eyebrow_lower_left_corner']['x'],res['right_eyebrow_lower_left_corner']['y']],
+        [res['right_eyebrow_lower_left_quarter']['x'],res['right_eyebrow_lower_left_quarter']['y']],
+        [res['right_eyebrow_lower_middle']['x'],res['right_eyebrow_lower_middle']['y']],
+        [res['right_eyebrow_lower_right_quarter']['x'],res['right_eyebrow_lower_right_quarter']['y']],
+        [res['mouth_left_corner']['x'],res['mouth_left_corner']['y']],
+        [res['mouth_upper_lip_left_contour2']['x'],res['mouth_upper_lip_left_contour2']['y']],
+        [res['mouth_upper_lip_left_contour4']['x'],res['mouth_upper_lip_left_contour4']['y']],
+        [res['mouth_upper_lip_top']['x'],res['mouth_upper_lip_top']['y']],
+        [res['mouth_upper_lip_right_contour3']['x'],res['mouth_upper_lip_right_contour3']['y']],
+        [res['mouth_upper_lip_right_contour2']['x'],res['mouth_upper_lip_right_contour2']['y']],
+        [res['mouth_right_corner']['x'],res['mouth_right_corner']['y']],
+        [res['mouth_lower_lip_right_contour2']['x'],res['mouth_lower_lip_right_contour2']['y']],
+        [res['mouth_lower_lip_right_contour3']['x'],res['mouth_lower_lip_right_contour3']['y']],
+        [res['mouth_lower_lip_bottom']['x'],res['mouth_lower_lip_bottom']['y']],
+        [res['mouth_lower_lip_left_contour3']['x'],res['mouth_lower_lip_left_contour3']['y']],
+        [res['mouth_lower_lip_left_contour2']['x'],res['mouth_lower_lip_left_contour2']['y']],
     ]
-    """
-        [res['mouth_left_corner']['x'], res['mouth_left_corner']['y']],
-        [res['mouth_lower_lip_bottom']['x'], res['mouth_lower_lip_bottom']['y']],
-        [res['mouth_lower_lip_left_contour1']['x'], res['mouth_lower_lip_left_contour1']['y']],
-        [res['mouth_lower_lip_left_contour2']['x'], res['mouth_lower_lip_left_contour2']['y']],
-        [res['mouth_lower_lip_left_contour3']['x'], res['mouth_lower_lip_left_contour3']['y']],
-        [res['mouth_lower_lip_right_contour1']['x'], res['mouth_lower_lip_right_contour1']['y']],
-        [res['mouth_lower_lip_right_contour2']['x'], res['mouth_lower_lip_right_contour2']['y']],
-        [res['mouth_lower_lip_right_contour3']['x'], res['mouth_lower_lip_right_contour3']['y']],
-        [res['mouth_lower_lip_top']['x'], res['mouth_lower_lip_top']['y']],
-        [res['mouth_right_corner']['x'], res['mouth_right_corner']['y']],
-        [res['mouth_upper_lip_bottom']['x'], res['mouth_upper_lip_bottom']['y']],
-        [res['mouth_upper_lip_left_contour1']['x'], res['mouth_upper_lip_left_contour1']['y']],
-        [res['mouth_upper_lip_left_contour2']['x'], res['mouth_upper_lip_left_contour2']['y']],
-        [res['mouth_upper_lip_left_contour3']['x'], res['mouth_upper_lip_left_contour3']['y']],
-        [res['mouth_upper_lip_right_contour1']['x'], res['mouth_upper_lip_right_contour1']['y']],
-        [res['mouth_upper_lip_right_contour2']['x'], res['mouth_upper_lip_right_contour2']['y']],
-        [res['mouth_upper_lip_right_contour3']['x'], res['mouth_upper_lip_right_contour3']['y']],
-        [res['mouth_upper_lip_top']['x'], res['mouth_upper_lip_top']['y']],   
-    """
 
     return pointer
